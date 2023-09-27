6af3ff8fb38dd84a67096ed47dbe62d31341d4f1
gustj3104 <gustj7038@gmail.com>
2023-09-27 23:39:25 +0900

    Feat.1주차 과제 코드

---
+++ week1/info.java
@@ -0,0 +1,8 @@
+public class info {
+    public static void main(String[] args){
+        System.out.println("이름: 윤현서");
+        System.out.println("학번: 2315029");
+        System.out.println("전공: 데이터사이언스");
+    }
+}
+
