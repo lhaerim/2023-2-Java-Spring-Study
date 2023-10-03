a0eff89aa9c8708137924d835079725bfe7db70f
이해림 <lhaerim1229@naver.com>
2023-10-03 15:59:00 +0900

    Move: 1주차 과제 코드 파일 이동

---
+++ 이해림/1주차/Alphabet.java
@@ -0,0 +1,10 @@
+public class Alphabet {
+    public static void main(String[] args) {
+        char c1 = 'A';
+
+        while (c1 <= 'Z') {
+            System.out.print(c1);
+            c1++;
+        }
+    }
+}
---
+++ 이해림/1주차/MyProfile.java
@@ -0,0 +1,14 @@
+public class MyProfile {
+    public static void main(String[] arags) {
+        String x = "이해림";
+        long y = 2313638;
+        String z = "컴퓨터과학";
+
+        System.out.print("이름: ");
+        System.out.println(x);
+        System.out.print("학번: ");
+        System.out.println(y);
+        System.out.print("전공: ");
+        System.out.println(z);
+    }
+}
